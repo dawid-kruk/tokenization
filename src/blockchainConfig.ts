1aa17da8c383744749f145993b421468c6bb9fd8
Dawid Kruk <dawid.kruk@ovoo.pl>
2023-04-14 11:56:41 +0200

    Update to use devnet

--- src/blockchainConfig.ts
+++ src/blockchainConfig.ts
@@ -2,10 +2,10 @@ export const blockchainConfig = {
   coinDenom: "C4E",
   coinMinimalDenom: "uc4e",
   coinDecimals: 6,
-  chainId: "veles-1",
+  chainId: "veles-dev-test-1",
   addressPrefix: "c4e",
-  rpcUrl: "https://rpc-testnet.c4e.io",
-  restUrl: "https://lcd-testnet.c4e.io/",
+  rpcUrl: "https://rpc-dev.c4e.io",
+  restUrl: "https://lcd-dev.c4e.io/",
   gasPrices: {
     average: 0.025,
     high: 0.03,
