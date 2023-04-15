9e9d494d6959fa32c422c591eee886dbf27a0f82
Dawid Kruk <dawid.kruk@ovoo.pl>
2023-04-15 15:50:21 +0200

    Update config to mainnet

--- src/blockchainConfig.ts
+++ src/blockchainConfig.ts
@@ -2,10 +2,10 @@ export const blockchainConfig = {
   coinDenom: "C4E",
   coinMinimalDenom: "uc4e",
   coinDecimals: 6,
-  chainId: "veles-dev-test-1",
+  chainId: "perun-1",
   addressPrefix: "c4e",
-  rpcUrl: "https://rpc-dev.c4e.io",
-  restUrl: "https://lcd-dev.c4e.io/",
+  rpcUrl: "https://rpc.c4e.io",
+  restUrl: "https://lcd.c4e.io/",
   gasPrices: {
     average: 0.025,
     high: 0.03,
